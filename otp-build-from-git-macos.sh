e82ab2b27924c2687f824b6ef203a34fce484d88
Kenji Rikitake <kenji.rikitake@acm.org>
2017-07-08 16:53:26 +0900

    Remove doc options from macOS script

--- otp-build-from-git-macos.sh
+++ otp-build-from-git-macos.sh
@@ -7,8 +7,5 @@ env \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j2" \
     KERL_BUILD_PLT="yes" \
-    KERL_BUILD_DOCS="yes" \
-    KERL_INSTALL_MANPAGES="yes" \
-    KERL_INSTALL_HTMLDOCS="yes" \
     KERL_CONFIGURE_OPTIONS="CC=/usr/local/opt/ccache/libexec/cc CXX=/usr/local/opt/ccache/libexec/c++ --enable-m64-build --enable-darwin-64bit --enable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --disable-native-libs --enable-kernel-poll --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=/usr/local/opt/openssl --disable-silent-rules --enable-dynamic-ssl-lib --enable-shared-zlib" \
     kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${OTP_VERSION}
