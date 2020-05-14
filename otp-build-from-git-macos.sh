6a839d3846c12e427ccc14bcc9ae2ce52cdd7565
Kenji Rikitake <kenji.rikitake@acm.org>
2020-05-14 15:14:05 +0900

    otp*.sh: add --enable-esock

--- otp-build-from-git-macos.sh
+++ otp-build-from-git-macos.sh
@@ -9,6 +9,6 @@ env \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j2" \
     KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--enable-m64-build --enable-darwin-64bit --disable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --disable-native-libs --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --disable-sctp --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL} --disable-silent-rules --disable-dynamic-ssl-lib" \
+    KERL_CONFIGURE_OPTIONS="--enable-m64-build --enable-darwin-64bit --disable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --disable-native-libs --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --disable-sctp --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL} --disable-silent-rules --disable-dynamic-ssl-lib" \
     KERL_BUILD_DOCS="yes" \
     kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${OTP_VERSION}
--- otp-build-from-git-ubuntu.sh
+++ otp-build-from-git-ubuntu.sh
@@ -9,6 +9,6 @@ env \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j2" \
     KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--enable-kernel-poll --disable-hipe --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --disable-sctp --disable-dynamic-ssl-lib --with-ssl=/opt/openssl/1.1.1 --without-javac --without-odbc --disable-silent-rules --with-wx-config=/usr/bin/wx-config" \
+    KERL_CONFIGURE_OPTIONS="--enable-kernel-poll --disable-hipe --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --disable-sctp --disable-dynamic-ssl-lib --with-ssl=/opt/openssl/1.1.1 --without-javac --without-odbc --disable-silent-rules --with-wx-config=/usr/bin/wx-config" \
     KERL_BUILD_DOCS="yes" \
     kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${OTP_VERSION}
