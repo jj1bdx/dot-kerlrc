00c2d69b7109d14402f0afa2fe6c3833ba92f95f
Kenji Rikitake <kenji.rikitake@acm.org>
2022-05-25 11:53:13 +0900

    Revise macOS installation flags

--- otp-build-from-git-macos.sh
+++ otp-build-from-git-macos.sh
@@ -5,16 +5,12 @@ OPENSSL_LOCAL="/usr/local/opt/openssl@1.1"
 INSTALL_DIR_PREFIX="/Users/kenji/otp/"
 INSTALL_NAME=${OTP_VERSION}
 #
-#local autoconf hack for building Erlang/OTP 24.0
-#Add
-#   PATH="/Users/kenji/bin/autoconf-2.69/bin:${PATH}" \
-# 
 env \
     CC=/usr/bin/clang \
     CXX=/usr/bin/clang++ \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j12" \
-    KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--enable-m64-build --enable-darwin-64bit --disable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --disable-native-libs --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --disable-sctp --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL} --disable-silent-rules --disable-dynamic-ssl-lib" \
     KERL_BUILD_DOCS="yes" \
+    KERL_BUILD_PLT="yes" \
+    KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --enable-darwin-64bit --enable-dynamic-ssl-lib --disable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL}"
     sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${INSTALL_NAME})"
