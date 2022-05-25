244c5c8d78a462b9d04409b2b5f58abfdf8eb349
Kenji Rikitake <kenji.rikitake@acm.org>
2022-05-25 05:25:42 +0000

    Update Ubuntu scripts

--- otp-install-from-git-ubuntu.sh
+++ otp-install-from-git-ubuntu.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-# For Ubuntu with custom OpenSSL
-# required packages: build-essential libncurses5-dev autoconf libwxgtk3.0-dev
-# required packages for documentation: xsltproc fop libxml2-utils
-INSTALL_DIR_PREFIX=/home/kenji/otp/
+# For Ubuntu with a local OpenSSL
 OTP_VERSION=$1
-OPENSSL_LOCAL=/opt/openssl/1.1.1o
+OPENSSL_LOCAL="/opt/openssl/1.1.1o"
+INSTALL_DIR_PREFIX="/home/kenji/otp/"
+INSTALL_NAME=${OTP_VERSION}
 env \
-    CC=/usr/lib/ccache/gcc \
-    CXX=/usr/lib/ccache/g++ \
+    CC=/usr/bin/cc \
+    CXX=/usr/bin/c++ \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j12" \
-    KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--disable-hipe --disable-vm-probes --disable-native-libs --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --disable-sctp --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL} --disable-silent-rules --disable-dynamic-ssl-lib" \
     KERL_BUILD_DOCS="yes" \
-    sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${OTP_VERSION} && kerl install ${OTP_VERSION} ${INSTALL_DIR_PREFIX}${OTP_VERSION})"
+    KERL_BUILD_PLT="yes" \
+    KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --enable-m64-build --disable-dynamic-ssl-lib --disable-hipe --disable-vm-probes --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL}" \
+    sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${INSTALL_NAME} && kerl install ${INSTALL_NAME} ${INSTALL_DIR_PREFIX}${INSTALL_NAME})"
