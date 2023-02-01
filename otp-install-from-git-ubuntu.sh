c43d97d320a0d6f158632455f093123dbb5b1d19
Kenji Rikitake <kenji.rikitake@acm.org>
2023-02-01 06:17:12 +0000

    otp-install-from-git-ubuntu.sh: Add KERL_DOC_TARGETS

--- otp-install-from-git-ubuntu.sh
+++ otp-install-from-git-ubuntu.sh
@@ -10,6 +10,7 @@ env \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j12" \
     KERL_BUILD_DOCS="yes" \
+    KERL_DOC_TARGETS="man html chunks" \
     KERL_BUILD_PLT="yes" \
     KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --enable-m64-build --disable-dynamic-ssl-lib --disable-hipe --disable-vm-probes --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL}" \
     sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${INSTALL_NAME} && kerl install ${INSTALL_NAME} ${INSTALL_DIR_PREFIX}${INSTALL_NAME})"
