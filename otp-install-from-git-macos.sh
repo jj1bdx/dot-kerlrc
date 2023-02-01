1d38381077dfdd071616d8649bd21cdb498f6bbe
Kenji Rikitake <kenji.rikitake@acm.org>
2023-02-01 15:16:02 +0900

    otp-install-from-git-macos.sh: Add KERL_DOC_TARGETS

--- otp-install-from-git-macos.sh
+++ otp-install-from-git-macos.sh
@@ -11,6 +11,7 @@ env \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
     MAKEFLAGS="-j12" \
     KERL_BUILD_DOCS="yes" \
+    KERL_DOC_TARGETS="man html chunks" \
     KERL_BUILD_PLT="yes" \
     KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --enable-darwin-64bit --enable-dynamic-ssl-lib --disable-hipe --enable-vm-probes --with-dynamic-trace=dtrace --enable-kernel-poll --enable-dirty-schedulers --enable-sharing-preserving --enable-lock-counter --enable-esock --without-javac --without-odbc --enable-threads --enable-smp-support --with-wx --with-ssl=${OPENSSL_LOCAL}" \
     sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${INSTALL_NAME} && kerl install ${INSTALL_NAME} ${INSTALL_DIR_PREFIX}${INSTALL_NAME})"
