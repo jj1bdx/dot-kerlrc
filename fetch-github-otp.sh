57509fc599765474c8a959ff8288b1b4e327fd48
Kenji Rikitake <kenji.rikitake@acm.org>
2023-09-22 19:45:23 +0900

    fetch-github-otp.sh: add initialize commands as comments

--- fetch-github-otp.sh
+++ fetch-github-otp.sh
@@ -4,6 +4,14 @@
 cd ~/src/otp
 echo "current directory: `pwd`"
 #git remote add upstream https://github.com/erlang/otp.git
+# See <https://stackoverflow.com/a/76173696/417862>
+# check .git/config content as:
+#   
+#   [remote "origin"]
+#   url = <url>
+#   fetch = +refs/heads/*:refs/remotes/origin/*
+# git fetch --all
+# git branch -a
 git fetch upstream && git checkout master && git merge upstream/master && git push origin master
 git fetch upstream -t && git push origin --tags
 git checkout maint && git merge upstream/maint && git push origin maint
