3a1df436e3a99bcd549698fbb1f6bc09dd7104af
Kenji Rikitake <kenji.rikitake@acm.org>
2023-09-22 19:51:36 +0900

    fetch-github-otp.sh: add more init commands

--- fetch-github-otp.sh
+++ fetch-github-otp.sh
@@ -3,15 +3,20 @@
 # if not, create one
 cd ~/src/otp
 echo "current directory: `pwd`"
-#git remote add upstream https://github.com/erlang/otp.git
 # See <https://stackoverflow.com/a/76173696/417862>
 # check .git/config content as:
 #   
-#   [remote "origin"]
+#   [remote "upstream"]
 #   url = <url>
 #   fetch = +refs/heads/*:refs/remotes/origin/*
+#
+# ^ summary: fetch all branches from upstream
+#
+# git remote add upstream https://github.com/erlang/otp.git
 # git fetch --all
 # git branch -a
+# git checkout -t upstream/master
+# git checkout -t upstream/maint
 git fetch upstream && git checkout master && git merge upstream/master && git push origin master
 git fetch upstream -t && git push origin --tags
 git checkout maint && git merge upstream/maint && git push origin maint
