715731682b5d31157787a2a61d5984a2bc7dbd06
Kenji Rikitake <kenji.rikitake@acm.org>
2023-12-15 10:43:15 +0000

    refactor build config on Ubuntu

--- otp-install-from-git-ubuntu.sh
+++ otp-install-from-git-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # For Ubuntu with a local OpenSSL
 OTP_VERSION=$1
-OPENSSL_LOCAL="/opt/openssl/3.1.3"
+OPENSSL_LOCAL="/opt/openssl/3.2.0"
 INSTALL_DIR_PREFIX="/home/kenji/otp/"
 INSTALL_NAME=${OTP_VERSION}
 #
@@ -9,9 +9,9 @@ env \
     CC=/usr/bin/cc \
     CXX=/usr/bin/c++ \
     CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
-    MAKEFLAGS="-j" \
+    MAKEFLAGS="-j8" \
     KERL_BUILD_DOCS="yes" \
-    KERL_DOC_TARGETS="man html chunks" \
+    KERL_DOC_TARGETS="chunks" \
     KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--disable-dynamic-ssl-lib --enable-kernel-poll --enable-dirty-schedulers --disable-sctp --disable-sharing-preserving --without-javac --without-odbc --with-ssl=${OPENSSL_LOCAL}" \
+    KERL_CONFIGURE_OPTIONS="--disable-dynamic-ssl-lib --enable-kernel-poll --enable-dirty-schedulers --disable-sctp --disable-sharing-preserving --without-javac --without-odbc --with-ssl=${OPENSSL_LOCAL} --with-wx-config=/opt/wx-3.2/bin/wx-config" \
     sh -c "(kerl build git https://github.com/jj1bdx/otp/ OTP-${OTP_VERSION} ${INSTALL_NAME} && kerl install ${INSTALL_NAME} ${INSTALL_DIR_PREFIX}${INSTALL_NAME})"
