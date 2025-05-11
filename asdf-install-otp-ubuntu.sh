fd4df0cd5ce942da8fb22428e5c6c7392aa39f8b
Kenji Rikitake <kenji.rikitake@acm.org>
2025-05-11 03:43:56 +0000

    asdf-install-otp-ubuntu.sh: remove unused/unstable tools
    * See <https://zenn.dev/voluntas/articles/erlang-source-code-install>

--- asdf-install-otp-ubuntu.sh
+++ asdf-install-otp-ubuntu.sh
@@ -1,18 +1,45 @@
 #!/bin/sh
-# For Ubuntu with a local OpenSSL
+# General build options
 OTP_VERSION=$1
 OPENSSL_LOCAL="/opt/openssl/3.5.0"
+CONFIGURE_OPTIONS=" \
+  --with-ssl=${OPENSSL_LOCAL} \
+  --enable-kernel-poll \
+  --enable-dirty-schedulers \
+  --enable-jit \
+  --disable-sctp \
+  --disable-dynamic-ssl-lib \
+  --disable-sharing-preserving \
+  --without-javac \
+  --without-odbc \
+  --without-wx \
+  --without-debugger \
+  --without-observer \
+  --without-crashdump_viewer \
+  --without-et \
+  --without-tftp \
+  --without-ftp \
+  --without-megaco \
+  --without-eldap \
+  --without-diameter \
+  --without-jinterface \
+  --without-snmp \
+  " 
 INSTALL_DIR_PREFIX="/home/kenji/otp/"
 INSTALL_NAME=${OTP_VERSION}
+#
+#WXCONFIG_LOCAL="/opt/wx-3.2/bin/wx-config"
+#CONFIGURE_OPTION_WX="--with-wx-config=${WXCONFIG_LOCAL}"
 #
 env \
     CC=/usr/bin/cc \
     CXX=/usr/bin/c++ \
-    CFLAGS="-O3 -fstack-protector-strong" LDFLAGS="-fstack-protector-strong" \
-    MAKEFLAGS="-j8" \
+    CFLAGS="-O3 -fstack-protector-strong" \
+    LDFLAGS="-fstack-protector-strong" \
+    MAKEFLAGS="-j$(nproc)" \
     OTP_GITHUB_URL="https://github.com/jj1bdx/otp/" \
     KERL_BUILD_DOCS="yes" \
     KERL_DOC_TARGETS="chunks" \
     KERL_BUILD_PLT="yes" \
-    KERL_CONFIGURE_OPTIONS="--disable-dynamic-ssl-lib --enable-kernel-poll --enable-dirty-schedulers --disable-sctp --disable-sharing-preserving --without-javac --without-odbc --with-ssl=${OPENSSL_LOCAL} --with-wx-config=/usr/bin/wx-config" \
+    KERL_CONFIGURE_OPTIONS="${CONFIGURE_OPTIONS}" \
     asdf install erlang ${OTP_VERSION}
